7dd5fc1fe9c67eb17961a1e3954ab78d756d929f
Vladimir <111503066+vladimirqw1221@users.noreply.github.com>
2023-08-21 22:50:59 +0400

    new feature

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def driver(request):
     options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=2560,1600")
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage ")
     options.add_argument("--disable-setuid-sandbox")
