3ff4a5393c5b35877a059737f7981c9aa38820d5
vladimirqw1221 <vladimirqw1221@users.noreply.github.com>
2023-08-21 21:00:43 +0000

    deploy: c828285ac81aa9e9177c22f75dcfa17ca5149257

--- data/data_site.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-class DataSite:
-    BASE_URL = os.getenv('BASE_URL')
--- src/wrongs/wrong_errors.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class GlobalWrong(Enum):
-    WRONG_TITLE = "It  title is not equal  "
-    WRONG_URL = "It url in not current "
