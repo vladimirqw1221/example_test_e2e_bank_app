5e6192ff7955cd7c7620a4f8cbcdc862ac9659e7
Vladimir <111503066+vladimirqw1221@users.noreply.github.com>
2024-03-04 16:35:10 +0400

    new fix sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 
-
-# Установить переменную окружения для указания пути к файлу .env
-export BASE_URL=https://www.globalsqa.com/angularJs-protractor/BankingProject/
-
-# Загрузить переменные окружения из файла .env
-
-
-# Запустить тесты с использованием pytest и передачей переменных окружения
 pytest
